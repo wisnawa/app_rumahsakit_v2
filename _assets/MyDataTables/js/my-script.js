8ed110c2ad1e037fb29679fffab0920dee94a206
wisnawa <putu.wisnawa@gmail.com>
2023-02-27 18:28:32 +0800

    modified

--- _assets/MyDataTables/js/my-script.js
+++ _assets/MyDataTables/js/my-script.js
@@ -29,5 +29,17 @@ $(document).ready(function () {
     processing: true,
     serverSide: true,
     ajax: "../pasien/pasien_data.php",
+    columnDefs: [
+      {
+        searchable: false,
+        orderable: false,
+        targets: [5],
+        render: function (data, type, row) {
+          const btn =
+            '<div style="text-align: center"><a href="edit.php?id="+data+"" class="btn btn-sm btn-outline-warning" style="margin-right: 9px;"><i class="fa-regular fa-pen-to-square"></i>&nbsp;Edit</a><a href="dell.php?id="+data+"" class="btn btn-sm btn-outline-danger" onclick="return confirm(\'Yakin dihapus?\')"><i class="fa-regular fa-trash-can"></i>&nbsp;Delete</a></div>';
+          return btn;
+        },
+      },
+    ],
   });
 });
--- pasien/data.php
+++ pasien/data.php
@@ -65,4 +65,24 @@
         <button class="btn btn-sm btn-outline-danger" id="btnDelete"><i class=" fa-regular fa-trash-can"></i>&nbsp;Hapus Data</button>
     </div>
 </div>
+<script>
+    // datatable server side
+    $(document).ready(function() {
+        $("#pasienTable").DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "pasien_data.php",
+            columnDefs: [{
+                searchable: false,
+                orderable: false,
+                targets: [5],
+                render: function(data, type, row) {
+                    const btn =
+                        '<div style="text-align: center"><a href="edit.php?id="+data+"" class="btn btn-sm btn-outline-warning" style="margin-right: 9px;"><i class="fa-regular fa-pen-to-square"></i>&nbsp;Edit</a><a href="dell.php?id="+data+"" class="btn btn-sm btn-outline-danger" onclick="return confirm(\'Yakin dihapus?\')"><i class="fa-regular fa-trash-can"></i>&nbsp;Delete</a></div>';
+                    return btn;
+                },
+            }, ],
+        });
+    });
+</script>
 <?php include_once('../_footer.php'); ?>
